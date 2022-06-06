0efb8bdb7a16a47df869b0b07c08e0a1334562d5
creativeartbd <creativeartbd@gmail.com>
2022-06-06 15:34:45 +0600

    Widget Content Tab

--- widgets/class-table-master.php
+++ widgets/class-table-master.php
@@ -35,8 +35,8 @@ class TableMaster extends Widget_Base {
 	public function __construct( $data = array(), $args = null ) {
 		parent::__construct( $data, $args );
 
-		wp_register_style( 'tablemaster', plugins_url( '/assets/css/tablemaster.css', ELEMENTOR_NAVIGATEMASTER ), array(), '1.0.0' );
-		wp_register_script( 'tablemaster', plugins_url( '/assets/js/tablemaster.js', ELEMENTOR_NAVIGATEMASTER ), array('jquery'), '1.0.0', true );
+		wp_register_style( 'tablemaster', plugins_url( '/assets/css/tablemaster.css', ELEMENTOR_TABLEMASTER ), array(), '1.0.0' );
+		wp_register_script( 'tablemaster', plugins_url( '/assets/js/tablemaster.js', ELEMENTOR_TABLEMASTER ), array('jquery'), '1.0.0', true );
 	}
 
 	/**
@@ -119,6 +119,10 @@ class TableMaster extends Widget_Base {
 	 */
 	protected function _register_controls() {
 
+		/* ===========================
+		Control Section Start Here
+		=========================== */
+
 		$this->start_controls_section(
 			'content_section',
 			[
@@ -127,342 +131,342 @@ class TableMaster extends Widget_Base {
 			]
 		);
 
+		$this->add_control(
+			'sh_details_heading_title', [
+				'label' => esc_html__( 'Details Heading', 'table-master' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'default' => esc_html__( 'Details Heading Name' , 'table-master' ),
+				'label_block' => true,
+			]
+		);
+
+		$this->add_control(
+			'sh_data_heading_title', [
+				'label' => esc_html__( 'Data Heading', 'table-master' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'default' => esc_html__( 'Data Heading Name' , 'table-master' ),
+				'label_block' => true,
+			]
+		);
+
 		$repeater = new \Elementor\Repeater();
 
 		$repeater->add_control(
-			'nm_title', [
-				'label' => esc_html__( 'Navigation Title', 'table-master' ),
+			'sh_details_title', [
+				'label' => esc_html__( 'Details Title', 'table-master' ),
 				'type' => \Elementor\Controls_Manager::TEXT,
-				'default' => esc_html__( 'Navigation Title' , 'table-master' ),
+				'default' => esc_html__( 'Default title name' , 'table-master' ),
 				'label_block' => true,
 			]
 		);
 
 		$repeater->add_control(
-			'nm_link', [
-				'label' => esc_html__( 'Navigation Link', 'table-master' ),
+			'sh_data_description', [
+				'label' => esc_html__( 'Data Description', 'table-master' ),
 				'type' => \Elementor\Controls_Manager::TEXT,
-				'default' => esc_html__( 'Navigation Link' , 'table-master' ),
+				'default' => esc_html__( 'Data description...' , 'table-master' ),
 				'label_block' => true,
 			]
 		);
 		
 		$this->add_control(
-			'nm_list',
+			'sh_table_info',
 			[
-				'label' => esc_html__( 'New Navigation', 'table-master' ),
+				'label' => esc_html__( 'Add table informaton', 'table-master' ),
 				'type' => \Elementor\Controls_Manager::REPEATER,
 				'fields' => $repeater->get_controls(),
 				'default' => [
 					[
-						'nm_title' => esc_html__( 'Navigation Title #1', 'table-master' ),
-						'nm_link' => esc_html__( 'nav1', 'table-master' ),
+						'sh_details_title' => esc_html__( 'Details Title #1', 'table-master' ),
+						'sh_data_description' => esc_html__( 'Data Description...', 'table-master' ),
 					],
 				],
-				'title_field' => '{{{ nm_title }}}',
-			]
-		);
-
-		$this->add_control(
-			'nm_nm_title',
-			[
-				'label' => esc_html__( 'Show Title', 'table-master' ),
-				'type' => \Elementor\Controls_Manager::SWITCHER,
-				'label_on' => esc_html__( 'Show', 'your-plugin' ),
-				'label_off' => esc_html__( 'Hide', 'your-plugin' ),
-				'return_value' => 'yes',
-				'default' => 'yes',
+				'title_field' => 'Add details and data description',
 			]
 		);
 
 		$this->end_controls_section();
 
+		/* ===========================
+		Control Section END Here
+		=========================== */
+
+		/* ===========================
+		Style Section Start Here
+		=========================== */
+
+		$this->end_controls_section();
 
 		$this->start_controls_section(
 			'style_section',
 			[
-				'label' => esc_html__( 'Style', 'table-master' ),
+				'label' => esc_html__( 'Heading', 'table-master' ),
 				'tab' => \Elementor\Controls_Manager::TAB_STYLE,
 			]
 		);
 
-		$this->add_control(
-			'nm_bg_color', [
-				'label' => esc_html__( 'Hover Color', 'table-master' ),
-				'type' => \Elementor\Controls_Manager::COLOR,
-				'default' => esc_html__( '#f00' , 'table-master' ),
-				'label_block' => true,
-				'selectors' => [
-					'{{WRAPPER}} .table-master ul li a:hover' => 'color: {{VALUE}} ',
-				],
-				'condition' => [
-					'nm_nm_title' => 'yes',
-				],
-			]
-		);
-
-		$this->add_group_control(
-			\Elementor\Group_Control_Background::get_type(),
-			[
-				'name' => 'background',
-				'label' => esc_html__( 'Background', 'table-master' ),
-				'types' => [ 'classic', 'gradient', 'video' ],
-				'selector' => '{{WRAPPER}} .table-master ul li a',
-				'default' => esc_html__( '#ffb200' , 'table-master' ),
-				'condition' => [
-					'nm_nm_title' => '',
-				],
-			]
-		);
-
-		$this->add_group_control(
-			\Elementor\Group_Control_Background::get_type(),
-			[
-				'name' => 'background_hover',
-				'label' => esc_html__( 'Background Hover', 'table-master' ),
-				'types' => [ 'classic', 'gradient', 'video' ],
-				'selector' => '{{WRAPPER}} .table-master ul li a:hover',
-				'default' => esc_html__( 'yellow' , 'table-master' ),
-				'condition' => [
-					'nm_nm_title' => '',
-				],
-			]
-		);
-
-		$this->add_group_control(
-			\Elementor\Group_Control_Background::get_type(),
-			[
-				'name' => 'background_active',
-				'label' => esc_html__( 'Background Active', 'table-master' ),
-				'types' => [ 'classic', 'gradient', 'video' ],
-				'selector' => '{{WRAPPER}} .table-master ul li.active a',
-				'default' => esc_html__( '#ffb200' , 'table-master' ),
-				'condition' => [
-					'nm_nm_title' => '',
-				],
-			]
-		);
-
-
 		$this->add_responsive_control(
-			'nm_margin',
+			'sh_heading_alignment',
 			[
-				'label' => esc_html__( 'Margin', 'table-master' ),
-				'type' => \Elementor\Controls_Manager::DIMENSIONS,
-				'size_units' => [ 'px', '%', 'em' ],
-				'selectors' => [
-					'{{WRAPPER}} .table-master ul li a' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				'label' => esc_html__( 'Heading Alignment', 'plugin-name' ),
+				'type' => \Elementor\Controls_Manager::CHOOSE,
+				'options' => [
+					'left' => [
+						'title' => esc_html__( 'Left', 'plugin-name' ),
+						'icon' => 'eicon-text-align-left',
+					],
+					'center' => [
+						'title' => esc_html__( 'Center', 'plugin-name' ),
+						'icon' => 'eicon-text-align-center',
+					],
+					'right' => [
+						'title' => esc_html__( 'Right', 'plugin-name' ),
+						'icon' => 'eicon-text-align-right',
+					],
 				],
-			]
-		);
-
-		$this->add_responsive_control(
-			'nm_padding',
-			[
-				'label' => esc_html__( 'Padding', 'table-master' ),
-				'type' => \Elementor\Controls_Manager::DIMENSIONS,
-				'size_units' => [ 'px', '%', 'em' ],
+				'default' => 'center',
+				'toggle' => true,
 				'selectors' => [
-					'{{WRAPPER}} .table-master ul li a' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+					'{{WRAPPER}} .sh_table_master table thead tr th' => 'text-align : {{VALUE}};',
 				],
-			]
-		);
-
-		$this->add_control(
-			'hr',
-			[
-				'type' => \Elementor\Controls_Manager::DIVIDER,
-			]
-		);
-
-		$this->add_control(
-			'nm_border_radius',
-			[
-				'label' => esc_html__( 'Border Radius', 'table-master' ),
-				'type' => \Elementor\Controls_Manager::DIMENSIONS,
-				'size_units' => [ 'px', '%', 'em' ],
-				'selectors' => [
-					'{{WRAPPER}} .table-master ul li a' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
-				],
+				// 'selector' => '{{WRAPPER}} .sh_table_master table thead tr th',
 			]
 		);
 
 		$this->add_group_control(
 			\Elementor\Group_Control_Typography::get_type(),
 			[
-				'name' => 'nm_content_typography',
-				'selector' => '{{WRAPPER}} .table-master ul li a',
+				'name' => 'sh_heading_typography',
+				'selector' => '{{WRAPPER}} .sh_table_master table thead tr th',
 			]
 		);
+
+		$this->add_control(
+			'more_options',
+			[
+				'label' => esc_html__( 'Heading Title Color', 'plugin-name' ),
+				'type' => \Elementor\Controls_Manager::HEADING,
+				'separator' => 'before',
+			]
+		);
+
+		$this->start_controls_tabs(
+			'sh_heading_color_tabs'
+		);
+
+			$this->start_controls_tab(
+				'sh_heading_title_normal_tab',
+				[
+					'label' => esc_html__( 'Normal', 'plugin-name' ),
+				]
+			);
+
+			$this->add_control(
+				'sh_heading_title_normal_color',
+				[
+					'label' => esc_html__( 'Heading Title Color', 'plugin-name' ),
+					'type' => \Elementor\Controls_Manager::COLOR,
+					'selectors' => [
+						'{{WRAPPER}} .sh_table_master table thead tr th' => 'color: {{VALUE}}',
+					],
+				]
+			);
+
+			$this->end_controls_tab();
+
+			$this->start_controls_tab(
+				'sh_heading_title_hover_tab',
+				[
+					'label' => esc_html__( 'Hover', 'plugin-name' ),
+				]
+			);
+
+			$this->add_control(
+				'sh_heading_title_hover_color',
+				[
+					'label' => esc_html__( 'Heading Title Hover Color', 'plugin-name' ),
+					'type' => \Elementor\Controls_Manager::COLOR,
+					'selectors' => [
+						'{{WRAPPER}} .sh_table_master table thead tr th:hover' => 'color: {{VALUE}}',
+					],
+				]
+			);
+	
+			$this->end_controls_tab();
+
+		$this->end_controls_tabs();
+	
+		$this->start_controls_tabs(
+			'sh_heading_background_tabs'
+		);
+
+			$this->start_controls_tab(
+				'sh_heading_background_normal_tab',
+				[
+					'label' => esc_html__( 'Normal', 'plugin-name' ),
+				]
+			);
+
+			$this->add_group_control(
+				\Elementor\Group_Control_Background::get_type(),
+				[
+					'name' => 'sh_normal_background',
+					'label' => esc_html__( 'Heading Background Color', 'plugin-name' ),
+					'types' => [ 'classic', 'gradient' ],
+					'selector' => '{{WRAPPER}} .sh_table_master table thead tr',
+				]
+			);
+
+			$this->end_controls_tab();
+
+			$this->start_controls_tab(
+				'sh_heading_background_hover_tab',
+				[
+					'label' => esc_html__( 'Hover', 'plugin-name' ),
+				]
+			);
+
+			$this->add_group_control(
+				\Elementor\Group_Control_Background::get_type(),
+				[
+					'name' => 'sh_hover_background',
+					'label' => esc_html__( 'Heading Background Color', 'plugin-name' ),
+					'types' => [ 'classic', 'gradient' ],
+					'selector' => '{{WRAPPER}} .sh_table_master table thead tr:hover',
+				]
+			);
+
+			$this->end_controls_tab();
+
+		$this->end_controls_tabs();
+		
+		$this->add_group_control(
+			\Elementor\Group_Control_Text_Shadow::get_type(),
+			[
+				'name' => 'sh_heading_shadow',
+				'label' => esc_html__( 'Heading Text Shadow', 'plugin-name' ),
+				'selector' => '{{WRAPPER}} .sh_table_master table thead tr th',
+			]
+		);
+
+		$this->start_controls_tabs(
+			'sh_heading_border_tabs'
+		);
+
+			$this->start_controls_tab(
+				'sh_heading_border_normal_tab',
+				[
+					'label' => esc_html__( 'Normal', 'plugin-name' ),
+				]
+			);
+
+			$this->add_group_control(
+				\Elementor\Group_Control_Border::get_type(),
+				[
+					'name' => 'sh_heading_normal_border',
+					'label' => esc_html__( 'Heading Border', 'plugin-name' ),
+					'selector' => '{{WRAPPER}} .sh_table_master table thead',
+				]
+			);
+
+			$this->end_controls_tab();
+
+			$this->start_controls_tab(
+				'sh_heading_border_hover_tab',
+				[
+					'label' => esc_html__( 'Hover', 'plugin-name' ),
+				]
+			);
+
+			$this->add_group_control(
+				\Elementor\Group_Control_Border::get_type(),
+				[
+					'name' => 'sh_heading_hover_border',
+					'label' => esc_html__( 'Heading Border', 'plugin-name' ),
+					'selector' => '{{WRAPPER}} .sh_table_master table thead:hover',
+				]
+			);
+
+			$this->end_controls_tab();
+
+		$this->end_controls_tabs();
 
 		$this->add_group_control(
 			\Elementor\Group_Control_Text_Shadow::get_type(),
 			[
-				'name' => 'nm_text_shadow',
-				'label' => esc_html__( 'Text Shadow', 'table-master' ),
-				'selector' => '{{WRAPPER}} .table-master ul li a',
+				'name' => 'sh_heading_shadow',
+				'label' => esc_html__( 'Heading Text Shadow', 'plugin-name' ),
+				'selector' => '{{WRAPPER}} .sh_table_master table thead tr th',
 			]
 		);
 
-		$this->add_control(
-			'hr',
-			[
-				'type' => \Elementor\Controls_Manager::DIVIDER,
-			]
+		$this->start_controls_tabs(
+			'sh_heading_opacity_tabs'
 		);
 
-		$this->add_group_control(
-			\Elementor\Group_Control_Border::get_type(),
-			[
-				'name' => 'nm_border',
-				'label' => esc_html__( 'Border', 'table-master' ),
-				'selector' => '{{WRAPPER}} .table-master ul li a',
-			]
-		);
+			$this->start_controls_tab(
+				'sh_heading_opacity_normal_tab',
+				[
+					'label' => esc_html__( 'Normal', 'plugin-name' ),
+				]
+			);
 
-		$this->add_control(
-			'hr',
-			[
-				'type' => \Elementor\Controls_Manager::DIVIDER,
-			]
-		);
-
-		$this->add_responsive_control(
-			'nm_width',
-			[
-				'label' => esc_html__( 'Width', 'table-master' ),
-				'type' => \Elementor\Controls_Manager::SLIDER,
-				'size_units' => [ 'px', '%' ],
-				'range' => [
-					'px' => [
-						'min' => 0,
-						'max' => 1000,
-						'step' => 5,
+			$this->add_responsive_control(
+				'sh_heading_normal_opacity',
+				[
+					'label' => esc_html__( 'Heading Opacity', 'navigate-master' ),
+					'type' => \Elementor\Controls_Manager::SLIDER,
+					'size_units' => [ 'px', '%' ],
+					'range' => [
+						'px' => [
+							'min' => 0,
+							'max' => 1,
+							'step' => .1,
+						],
 					],
-					'%' => [
-						'min' => 0,
-						'max' => 100,
+					'selectors' => [
+						'{{WRAPPER}} .sh_table_master table thead' => 'opacity: {{SIZE}};',
 					],
-				],
-				'default' => [
-					'unit' => 'px',
-					'size' => 100,
-				],
-				'selectors' => [
-					'{{WRAPPER}} .table-master ul li a' => 'width: {{SIZE}}{{UNIT}};',
-				],
-			]
-		);
+				]
+			);
+	
+			$this->end_controls_tab();
 
-		$this->add_responsive_control(
-			'nm_height',
-			[
-				'label' => esc_html__( 'Height', 'table-master' ),
-				'type' => \Elementor\Controls_Manager::SLIDER,
-				'size_units' => [ 'px', '%' ],
-				'range' => [
-					'px' => [
-						'min' => 0,
-						'max' => 1000,
-						'step' => 5,
-					],
-					'%' => [
-						'min' => 0,
-						'max' => 100,
-					],
-				],
-				'default' => [
-					'unit' => 'px',
-					'size' => 30,
-				],
-				'selectors' => [
-					'{{WRAPPER}} .table-master ul li a' => 'height: {{SIZE}}{{UNIT}};',
-				],
-			]
-		);
+			$this->start_controls_tab(
+				'sh_heading_opacity_hover_tab',
+				[
+					'label' => esc_html__( 'Hover', 'plugin-name' ),
+				]
+			);
 
-		$this->add_control(
-			'hr',
-			[
-				'type' => \Elementor\Controls_Manager::DIVIDER,
-			]
-		);
-
-
-		$this->add_responsive_control(
-			'nv_position',
-			[
-				'type' => \Elementor\Controls_Manager::SELECT,
-				'label' => esc_html__( 'Navigation Position', 'table-master' ),
-				'options' => [
-					'default' => esc_html__( 'Default', 'table-master' ),
-					'absolute' => esc_html__( 'Absolute', 'table-master' ),
-					'fixed' => esc_html__( 'Fixed', 'table-master' ),
-				],
-				'default' => 'fixed',
-				'selectors' => [
-					'{{WRAPPER}} .table-master' => 'position: {{VALUE}};',
-				],
-			]
-		);
-
-		$this->add_responsive_control(
-			'nm_position_right',
-			[
-				'label' => esc_html__( 'Right', 'table-master' ),
-				'type' => \Elementor\Controls_Manager::SLIDER,
-				'size_units' => [ 'px', '%' ],
-				'range' => [
-					'px' => [
-						'min' => 0,
-						'max' => 1000,
-						'step' => 5,
+			$this->add_responsive_control(
+				'sh_heading_hover_opacity',
+				[
+					'label' => esc_html__( 'Heading Opacity', 'navigate-master' ),
+					'type' => \Elementor\Controls_Manager::SLIDER,
+					'size_units' => [ 'px', '%' ],
+					'range' => [
+						'px' => [
+							'min' => 0,
+							'max' => 1,
+							'step' => .1,
+						],
 					],
-					'%' => [
-						'min' => 0,
-						'max' => 100,
+					'selectors' => [
+						'{{WRAPPER}} .sh_table_master table thead:hover' => 'opacity: {{SIZE}};',
 					],
-				],
-				'default' => [
-					'unit' => 'px',
-					'size' => 50,
-				],
-				'selectors' => [
-					'{{WRAPPER}} .table-master' => 'right: {{SIZE}}{{UNIT}};',
-				],
-			]
-		);
-
-		$this->add_responsive_control(
-			'nm_position_top',
-			[
-				'label' => esc_html__( 'Top', 'table-master' ),
-				'type' => \Elementor\Controls_Manager::SLIDER,
-				'size_units' => [ 'px', '%' ],
-				'range' => [
-					'px' => [
-						'min' => 0,
-						'max' => 1000,
-						'step' => 5,
-					],
-					'%' => [
-						'min' => 0,
-						'max' => 100,
-					],
-				],
-				'default' => [
-					'unit' => 'px',
-					'size' => 150,
-				],
-				'selectors' => [
-					'{{WRAPPER}} .table-master' => 'top: {{SIZE}}{{UNIT}};',
-				],
-			]
-		);
+				]
+			);
 	
 
-		$this->end_controls_section();
+			$this->end_controls_tab();
 
+		$this->end_controls_tabs();
+
+		
+		/* ===========================
+		Style Section END Here
+		=========================== */
 	}
 
 	/**
@@ -480,26 +484,47 @@ class TableMaster extends Widget_Base {
 		$this->add_inline_editing_attributes( 'title', 'none' );
 		$this->add_inline_editing_attributes( 'description', 'basic' );
 		$this->add_inline_editing_attributes( 'content', 'advanced' );
-	
 
-		$lists = $settings['nm_list'];
-		$nm_nm_title = $settings['nm_nm_title'];
+		$sh_details_heading_title = $settings['sh_details_heading_title'];
+		$sh_data_heading_title = $settings['sh_data_heading_title'];
+		$sh_table_info = $settings['sh_table_info'];
+		$sh_heading_alignment = $settings['sh_heading_alignment'];
+
 		?>
+		<style>
+			/* @media only screen and (max-width: 760px), (min-device-width: 768px) and (max-device-width: 1024px)  {
+				.sh_table_master table td:nth-of-type(1):before { content: "<?php echo $sh_details_heading_title; ?>"; }
+				.sh_table_master table td:nth-of-type(2):before { content: "<?php echo $sh_data_heading_title; ?>"; }
+			} */
+		</style>
+		<?php
 
-		<div class="table-master">
-			<ul>
-				<?php
-				foreach( $lists as $list )  {
-					if( 'yes' === $nm_nm_title ) {
-						echo "<li><a href='#".$list['nm_link']."'>".$list['nm_title']."</a></li>";
-					} else {
-						echo "<li><a href='#".$list['nm_link']."'>&nbsp;</a></li>";
+		if( !empty( $sh_details_heading_title ) && !empty( $sh_data_heading_title) ) {
+			echo "<div class='sh_table_master'>";
+			echo "<table>";
+				echo "<thead>";
+					echo "<tr>";
+						echo "<th>$sh_details_heading_title</th>";
+						echo "<th>$sh_data_heading_title</th>";
+					echo "</tr>";
+				echo "</thead>";
+				echo "<tbody>";
+				if( !empty( $sh_table_info ) ) {
+
+					foreach( $sh_table_info as $sh_info ) {
+
+						$sh_details_title = $sh_info['sh_details_title'];
+						$sh_data_description = $sh_info['sh_data_description'];
+
+						echo "<tr>";
+							echo "<td>$sh_details_title</td>";
+							echo "<td>$sh_data_description</td>";
+						echo "</tr>";
 					}
 				}
-				?>
-			</ul>
-		</div>
-		<?php
+				echo "</tbody>";
+			echo "</table>";
+			echo "</div>";
+		}
 	}
-
 }
